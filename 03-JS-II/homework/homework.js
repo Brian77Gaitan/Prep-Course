685a170747f54cb0656fadc840708bc746e30be1
Brian77Gaitan <brian_9510@hotmail.com>
2022-08-10 19:34:26 -0500

    03

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -30,7 +30,9 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-
+  if (status === 1) return "Online"; 
+  if (status === 2) return "Away";
+  return "Offline";
 }
 
 function saludo(idioma) {
@@ -40,6 +42,10 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman") return "Guten Tag!";
+  if (idioma === "mandarin") return "Ni Hao!";
+  if (idioma === "ingles") return "Hello!";
+  return "Hola!";
 }
 
 function colors(color) {
@@ -50,18 +56,39 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color) {
+    case "blue":
+      return "This is blue";
+      break;
+    case "red":
+      return "This is red";
+      break;
+    case "green":
+      return "This is green";
+      break;
+    case "orange":
+      return "This is orange";
+      break;
+    default:
+      return "Color not found";
+  }
+    
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero == 5) return true;
+  return false;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero < 50 && numero > 20) return true;
+  return false; 
 }
 
 function esEntero(numero) {
